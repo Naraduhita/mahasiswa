887b81d4987d584fb7d17d3057d107b96b6995d5
Naraduhita <brigitanara97@gmail.com>
2022-11-10 23:24:13 +0700

    add total

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,73 +2,32 @@
   <div class="">
     <div class="m-auto p-5">
       <!-- @submit.prevent="addShopp"  -->
-      <button
-        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-3 rounded"
-        @click.prevent="addRandomCode"
-      >
-        +
-      </button>
+      <div class="flex">
+      <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-3 rounded" @click.prevent="addRandomCode">+</button>
+      <div class="ml-3 my-auto font-bold text-xl">{{ total }}</div>
+    </div>
       <!--show Random Code -->
       <div id="randomCode">
-        <div v-for="item in arrayShopping" class="flex my-5">
+        <div v-for="item in arrayShopping" class="flex my-5" v-bind:key="item.id">
           <div class="border p-2 mr-5 w-1/5">{{ item.id }}</div>
-          <button
-            class="bg-purple5 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded"
-          >
+          <button class="bg-purple5 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded">
             {{ item.jumlah }}
           </button>
-          <button
-            class="bg-purple4 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded"
-            @click="penjumlahan(item.id)"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-              />
+          <button class="bg-purple4 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded" @click="penjumlahan(item.id)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
             </svg>
           </button>
           <!--Kurang-->
-          <button
-            class="bg-purple5 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded"
-            @click="pengurangan(item.id)"
-            :disabled="item.jumlah === 0"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-dash"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"
-              />
+          <button class="bg-purple5 mr-3 hover:bg-purple2 text-white font-bold py-2 px-4 rounded" @click="pengurangan(item.id)" :disabled="item.jumlah === 0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
             </svg>
           </button>
           <!-- Hapus -->
-          <button
-            class="bg-red-500 hover:bg-purple2 text-white font-bold py-2 px-4 rounded"
-            @click="hapus(item.id)"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-trash"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-              />
+          <button class="bg-red-500 hover:bg-purple2 text-white font-bold py-2 px-4 rounded" @click="hapus(item.id)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
               <path
                 fill-rule="evenodd"
                 d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
@@ -82,37 +41,28 @@
 </template>
 
 <script>
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  updateDoc,
-  deleteDoc,
-  where,
-  query,
-} from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, where, query } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5iy0fdXeQoVvedq8APCW2eVt8MailuAA",
-  authDomain: "shopp-app-b2b94.firebaseapp.com",
-  projectId: "shopp-app-b2b94",
-  storageBucket: "shopp-app-b2b94.appspot.com",
-  messagingSenderId: "139605541996",
-  appId: "1:139605541996:web:fdbcee58162ef53694c449",
-  measurementId: "G-XH00CV6D02",
+  apiKey: 'AIzaSyB5iy0fdXeQoVvedq8APCW2eVt8MailuAA',
+  authDomain: 'shopp-app-b2b94.firebaseapp.com',
+  projectId: 'shopp-app-b2b94',
+  storageBucket: 'shopp-app-b2b94.appspot.com',
+  messagingSenderId: '139605541996',
+  appId: '1:139605541996:web:fdbcee58162ef53694c449',
+  measurementId: 'G-XH00CV6D02',
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export default {
-  name: "Shopping",
+  name: 'Shopping',
   components: {},
   data() {
     return {
       arrayShopping: [],
+      total: 0,
     };
   },
 
@@ -126,18 +76,24 @@ export default {
         return id === item.id;
       });
       this.arrayShopping[index].jumlah = this.arrayShopping[index].jumlah + 1;
+      this.total = this.total + 1;
     },
     pengurangan(id) {
       const index = this.arrayShopping.findIndex((item) => {
         return id === item.id;
       });
       this.arrayShopping[index].jumlah = this.arrayShopping[index].jumlah - 1;
+      this.total = this.total - 1;
     },
-    hapus(id){
-      const arrayDeleted = this.arrayShopping.filter((item)=>item.id !== id)
-      this.arrayShopping = arrayDeleted
+    hapus(id) {
+      const index = this.arrayShopping.findIndex((item) => {
+        return id === item.id;
+      });
+      this.total = this.total - this.arrayShopping[index].jumlah;
+      const arrayDeleted = this.arrayShopping.filter((item) => item.id !== id);
+      this.arrayShopping = arrayDeleted;
       // console.log(this.arrayShopping)
-    }
+    },
   },
 };
 </script>
